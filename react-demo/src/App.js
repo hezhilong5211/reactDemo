7a716aadd274576d11e7c6db87400cd77750a667
hezhilong5211 <1176090144@qq.com>
2020-04-06 18:35:37 +0800

    antd install && global

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Button, message} from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+    handleClick =  () =>{
+        message.success('我是何志龙！！！！')
+    }
+
+    render(){
+        return(
+            <div>
+                <Button type="primary" onClick={this.handleClick}>何志龙</Button>
+            </div>
+        )
+    }
 }
-
-export default App;
