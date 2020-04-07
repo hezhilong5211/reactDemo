9131e41ac344a9bb4530e493fe8d2adba3659a9b
hezhilong1 <hezhilong1@100tal.com>
2020-04-07 14:33:29 +0800

    rules change

--- react-demo/src/pages/login/login.jsx
+++ react-demo/src/pages/login/login.jsx
@@ -6,9 +6,26 @@ import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 export default class login extends React.Component {
 
-
+    //对密码进行自定义验证
+    validatePwd = (rule, value, callback) => {
+        //callback 必须背调
+        if (value === '') {
+            callback('请输入密码');
+        } else if (value.length < 4) {
+            callback('密码长度不能小于4')
+        } else if (value.length > 12) {
+            callback('密码的长度不能大于12')
+        } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+            callback('mima')
+        } else {
+            callback()
+        }
+        // callback()
+        // callback('验证失败')
+    }
 
     render() {
+
         return (
             <div className="login">
                 <header className="login-header">
@@ -18,39 +35,41 @@ export default class login extends React.Component {
                 </header>
                 <section className="login-section">
                     <h2>用户登录</h2>
-                    <div>
-                        <Form
-                            name="normal_login"
-                            className="login-form"
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                    >
+                        <Form.Item
+                            name="username"
+                            //声明示验证 直接用别人写好的代码规则进行验证
+                            //自定义验证
+                            rules={[
+                                { required: true, message: '请输入用户名' },
+                                { min: 4, message: '用户名最少4位' },
+                                { max: 12, message: '用户名最多12位' },
+                                { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文下划线数字' }
+                            ]}
                         >
-                            <Form.Item
-                                name="username"
-                                rules={[{ required: true, message: 'Please input your Username!' }]}
-                            >
-                                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                            </Form.Item>
-                            <Form.Item
-                                name="password"
-                                rules={[{ required: true, message: 'Please input your Password!' }]}
-                            >
-                                <Input
-                                    prefix={<LockOutlined className="site-form-item-icon" />}
-                                    type="password"
-                                    placeholder="Password"
-                                />
-                            </Form.Item>
-                            <Form.Item>
-                                <Form.Item name="remember" valuePropName="checked" noStyle>
-                                </Form.Item>
-                            </Form.Item>
-
-                            <Form.Item>
-                                <Button type="primary" htmlType="submit" className="login-form-button">
-                                    Log in
-        </Button>
-                            </Form.Item>
-                        </Form>
-                    </div>
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                { validator: this.validatePwd }
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </section>
             </div>
         )
